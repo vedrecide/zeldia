bae8f37261ecff8bdf6dc4a66492f8750fe16a29
vedrecide <mohammedrayyan112233@gmail.com>
2022-11-03 19:20:55 +0530

    Added _about.py

---
+++ zeldia/_about.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+from typing import Final
+
+
+__author__: Final[str] = "vedrecide"
+__maintainer__: Final[str] = "vedrecide"
+__copyright__: Final[str] = "© 2022-present vedrecide"
+__issue_tracker__: Final[str] = "https://github.com/vedrecide/zeldia/issues"
+__license__: Final[str] = "MIT"
+__url__: Final[str] = "https://github.com/vedrecide/zeldia"
+__version__: Final[str] = "0.0.1"
